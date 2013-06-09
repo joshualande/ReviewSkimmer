c93bc84df69a35cd8fa66ceb4d506495440c2b5a
Joshua Lande <joshualande@gmail.com>
2013-06-09 00:25:13 -0700

    adding code to pull in top movies

---
+++ reviewskim/utils/strings.py
@@ -0,0 +1,6 @@
+def clean_unicode(string):
+    """ Convert unicode to string.
+        Note, replace unicode dash with regular dash. """
+    string=string.replace(u'\x96','-')
+    string=string.replace(u'\x97','-')
+    return unicode(string)
