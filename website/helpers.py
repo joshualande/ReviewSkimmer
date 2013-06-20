5d8fa6480765ae9c8cc5a7b7d4082e0b0e108cea
Joshua Lande <joshualande@gmail.com>
2013-06-19 20:13:01 -0700

    Updating front-end to the website

---
+++ website/helpers.py
@@ -0,0 +1,24 @@
+import urllib
+from collections import OrderedDict
+
+def get_search_url(imdb_movie_id,connector):
+    movie_name=connector.get_movie_name(imdb_movie_id)
+    url='/search.html?q='+urllib.quote(movie_name)
+    print 'dog',url
+    return url
+
+def get_poster_thumbnail(imdb_movie_id,connector):
+    path='https://s3-us-west-2.amazonaws.com/reviewskimmer/poster_thumbnail_%d.jpg' % imdb_movie_id
+    url=get_search_url(imdb_movie_id,connector)
+    return '<a href="%s"><img src="%s"></a>' % (url,path)
+
+def get_top_grossing_dict(connector, years, movies_per_year):
+
+    top_grossing=connector.get_top_grossing()
+    movies=OrderedDict()
+    for year in years:
+        temp=top_grossing[top_grossing['rs_year']==year]
+        temp.sort('rs_ranking')
+        m=temp['rs_imdb_movie_id'][:4].tolist()
+        movies[year]=[get_poster_thumbnail(i,connector) for i in m]
+    return movies
