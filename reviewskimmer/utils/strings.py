743300eae1dc12f26ffda0c66e750bc58c3ba1e3
Joshua Lande <joshualande@gmail.com>
2013-07-06 17:15:47 -0700

    Fixing unicode bug + about page

--- reviewskimmer/utils/strings.py
+++ reviewskimmer/utils/strings.py
@@ -1,6 +1,13 @@
+import unicodedata
+
 def clean_unicode(string):
     """ Convert unicode to string.
         Note, replace unicode dash with regular dash. """
     string=string.replace(u'\x96','-')
     string=string.replace(u'\x97','-')
     return string.encode('utf8').strip()
+
+def strip_unicode(string):
+    # from http://stackoverflow.com/questions/1342000/how-to-replace-non-ascii-characters-in-string
+    return "".join(i for i in string if ord(i)<128)
+
